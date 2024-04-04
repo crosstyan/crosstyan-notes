eeac40b84ae99ca33a7cdb2e0b32809b695b8000
crosstyan <crosstyan@outlook.com>
2024-04-04 15:30:54 +0800

    update
    Former-commit-id: 88ed418419000e92c185a67249e624ca95f41cb6

--- on_types/build.sh
+++ on_types/build.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-asciidoctor-pdf -r asciidoctor-mathematical -a mathematical-format=svg -o on_types.pdf $SCRIPT_DIR/README.adoc
+asciidoctor-pdf -r asciidoctor-mathematical -a mathematical-format=svg -o $SCRIPT_DIR/on_types.pdf $SCRIPT_DIR/README.adoc
 rm $SCRIPT_DIR/*.svg 
